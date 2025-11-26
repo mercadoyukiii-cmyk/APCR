4b1f8b4d85c6df617893a3b5eec296e566e726fd
mercadoyukiii-cmyk <mercadoyukiii@gmail.com>
2025-11-26 21:58:46 +0800

    Add files via upload

---
+++ HotelReservationdb/StoredProcedure/DeleteGuestAccount.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[DeleteGuestAccount]
+    @ClientId INT = NULL
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+        DELETE FROM [dbo].[Account]
+        WHERE ClientId = @ClientId;
+
+        DELETE FROM [dbo].[Client]
+        WHERE Id = @ClientId;
+END;
